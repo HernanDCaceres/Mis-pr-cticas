769d973d17cce55bd4f36b2d6f07d4b4a54b3c44
HernanD <projectmgrex@hotmail.com>
2024-04-14 18:18:49 -0500

    JEST testing

--- Practicas/Práctica 14 - JEST/demo-jest/tests/prueba.test.js
+++ Practicas/Práctica 14 - JEST/demo-jest/tests/prueba.test.js
@@ -1,5 +1,16 @@
-describe("Una prueba de tests que pasan siempre", () => {
-    it('Este test debe pasar siempre', () => {
-        expect(true).toBe(true);
+const { calcularTotal } = require("../index");
+
+const mockGetItems = jest.fn(() => {
+    return [
+        {name: "Producto A", price: 10, quantity: 2},
+        {name: "Producto B", price: 5, quantity: 3},
+        ];
+});
+
+describe("En la función calcularTotal", () => {
+
+    it('la función "getItems" que paso como parámetro debe ejecutarse al menos una vez', () => {
+        calcularTotal(mockGetItems);
+        expect(mockGetItems).toHaveBeenCalled();
     });
 });
