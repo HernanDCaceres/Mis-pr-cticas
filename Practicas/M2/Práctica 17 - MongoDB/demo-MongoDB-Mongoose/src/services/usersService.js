322ffe027202e2757325b23a48a49b0d05a40fad
HernanD <projectmgrex@hotmail.com>
2024-04-22 21:34:52 -0500

    Fin de demo de Mongoose

--- Practicas/M2/Práctica 17 - MongoDB/demo-MongoDB-Mongoose/src/services/usersService.js
+++ Practicas/M2/Práctica 17 - MongoDB/demo-MongoDB-Mongoose/src/services/usersService.js
@@ -1,17 +1,11 @@
-const users = [
-    {
-        name: "John Doe",
-        email: "jdoe@mail.com",
-    },
-];
+const User = require("../models/User");
 
 module.exports = {
     getUsers: async () => {
+        const users = await User.find();
         return users;
     },
 
     createUser: async (name, email) => {
-        users.push({ name,email });
-        return { name, email };
     },
 };
